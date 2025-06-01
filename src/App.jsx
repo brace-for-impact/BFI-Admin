7bd29339c99fbb86eb349bbfce5e284896094e0c
Selva Ganesh M <selva.ganesh@ogi-voos.com>
2025-06-02 00:43:08 +0530

    config: tailwind setup. fixes #2

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ function App() {
   return (
     <>
       <div>
+          <h1 class="text-3xl font-bold underline text-red-600">    Hello world!  </h1>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
